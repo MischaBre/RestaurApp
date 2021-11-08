7be624ce3a661cea00c68f43a100954b0a9819da
MischaGe <gemsa.michael@gmail.com>
2021-11-08 17:52:51 +0100

    Minor changes to UI

--- RestaurApp/Views/AddView.swift
+++ RestaurApp/Views/AddView.swift
@@ -17,56 +17,55 @@ struct AddView: View {
     @State var region: MKCoordinateRegion
     
     var body: some View {
-        
-        VStack(alignment: .center, spacing: 10) {
-            Text("Neues Restaurant")
-                .multilineTextAlignment(.center)
-            
-            HStack {
-                Button("Abbrechen") {
-                    restaurantListVM.name = ""
-                    selection.selection = 1
-                }
-                .foregroundColor(.red)
-                .padding()
-                Spacer()
-                Button("Speichern") {
-                    restaurantListVM.lat = $region.center.latitude.wrappedValue
-                    restaurantListVM.lon = $region.center.longitude.wrappedValue
-                    restaurantListVM.save()
-                    restaurantListVM.name = ""
-                    restaurantListVM.category = ""
-                    restaurantListVM.lat = 0.0
-                    restaurantListVM.lon = 0.0
-                    selection.selection = 1
-                }.padding()
-            }
-            
-            TextField("Name", text: $restaurantListVM.name)
-                .textFieldStyle(.roundedBorder)
-            
-            TextField("Kategorie", text: $restaurantListVM.category)
-                .textFieldStyle(.roundedBorder)
-            
-            HStack {
-                Text("Ort des Restaurants")
-                Spacer()
-            }
-            
-            ZStack {
-                
-                Map(coordinateRegion: $region, showsUserLocation: true)
-                    .onAppear {
-                        locationVM.getRegion()
-                        region = locationVM.region
+        NavigationView {
+            VStack(alignment: .center, spacing: 10) {
+                HStack {
+                    Button("Abbrechen") {
+                        restaurantListVM.name = ""
+                        selection.selection = 1
                     }
-                    .frame(height: 250)
-                    .overlay(
-                        Image(systemName: "circle")
-                    )
+                    .foregroundColor(.red)
+                    .padding()
+                    Spacer()
+                    Button("Speichern") {
+                        restaurantListVM.lat = $region.center.latitude.wrappedValue
+                        restaurantListVM.lon = $region.center.longitude.wrappedValue
+                        restaurantListVM.save()
+                        restaurantListVM.name = ""
+                        restaurantListVM.category = ""
+                        restaurantListVM.lat = 0.0
+                        restaurantListVM.lon = 0.0
+                        selection.selection = 1
+                    }.padding()
+                }
+                
+                TextField("Name", text: $restaurantListVM.name)
+                    .textFieldStyle(.roundedBorder)
+                
+                TextField("Kategorie", text: $restaurantListVM.category)
+                    .textFieldStyle(.roundedBorder)
+                
+                HStack {
+                    Text("Ort des Restaurants")
+                    Spacer()
+                }
+                
+                ZStack {
+                    
+                    Map(coordinateRegion: $region, showsUserLocation: true)
+                        .onAppear {
+                            locationVM.getRegion()
+                            region = locationVM.region
+                        }
+                        .frame(height: 250)
+                        .overlay(
+                            Image(systemName: "circle")
+                        )
+                }
+                Spacer()
             }
-            Spacer()
+            .padding()
+            .navigationBarTitle("Neues Restaurant")
         }
-        .padding()
     }
 }
